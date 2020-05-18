2a9e43463611f68b128b169dfb19ac4981fbd6cd
akhilmhdh <akhilmhdh@gmail.com>
2020-05-18 19:28:59 +0530

    =added db support

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 # Create your models here.
+class Gallery(models.Model):
+    insta_id = models.CharField(max_length=50)
+    profile_pic = models.ImageField(default="default.jpg",
+                                    upload_to="profile_pics")
+    submission = models.CharField(max_length=1000)
+    thumbnail = models.ImageField(default="default.jpg",
+                                  upload_to="thumbnails")
