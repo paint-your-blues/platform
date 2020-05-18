4116057a2bcb32fc4b708cba8028f1f0a453db40
MonkeyScript <monkeyscript.dev@gmail.com>
2020-05-18 12:33:27 +0530

    Add modal for small devices

--- details/static/gallery/scripts/leaderboard.js
+++ details/static/gallery/scripts/leaderboard.js
@@ -1,3 +1,6 @@
+// Get device width 
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
 // Hide frame by default
 document.getElementById('framePreview').style.display = 'none';
 var listItems = document.getElementsByClassName("leaderboard-list-item");
@@ -5,7 +8,13 @@ var i;
 // Iterate over accordion and add click listeners
 for (i = 0; i < listItems.length; i++) {
     listItems[i].addEventListener("click", function () {
-        showFrame();
+        if(width>1024){
+            // Toggle frame on the side 
+            showFrame();
+        }else {
+            // Toggle modal 
+            showModal();
+        }
         setItemsInactive();   
         this.classList.add("active");
     });
@@ -20,4 +29,30 @@ function setItemsInactive() {
     for (i = 0; i < listItems.length; i++) {
         listItems[i].classList.remove("active");
     }
+}
+
+//
+// Modal controls
+//
+// Get the modal
+var modal = document.getElementById("previewModal");
+// Function to show modal
+function showModal() {
+    modal.style.display = "flex";
+}
+// Modal close btn
+var modalCloseBtn = document.getElementById("modalCloseBtn");
+// When the user clicks on the button, open the modal
+modalCloseBtn.onclick = function() {
+    modal.style.display = "none";
+    // Reset selection 
+    setItemsInactive();
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    // Reset selection 
+    setItemsInactive();
+  }
 }
