ddf57e47157be9ed947920263bed827442b62ae4
akhilmhdh <akhilmhdh@gmail.com>
2020-08-02 11:27:45 +0530

    added json res

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,8 @@
-from .views import EntryPageView, LeaderBoardView
+from .views import EntryPageView, LeaderBoardView, GalleryJSON
 from django.urls import path
 
 urlpatterns = [
     path('leaderboard/', LeaderBoardView, name='leader-page'),
+    path('gallery/json/', GalleryJSON, name='galley-json'),
     path('', EntryPageView, name='entry-page'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,45 +1,75 @@
 from django.shortcuts import render
 from django.db import transaction
+from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models.fields.files import ImageFieldFile
 from .models import Gallery
 import requests
 import threading
 import random
+import json
+
+
+class ExtendedEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, ImageFieldFile):
+            return str(o)
+        else:
+            return super().default(o)
+
+
+def GalleryJSON(req):
+    entries = (list(Gallery.objects.exclude(entry_id="0").order_by('-likes')))
+    gallery = []
+    for index, val in enumerate(entries):
+        gallery.append({
+            "standing": index,
+            "profile": val.profile_pic.url,
+            "thumbnail": val.thumbnail,
+            "insta_id": val.insta_id,
+            "post": val.post,
+            "entry_id": val.entry_id,
+            "caption": val.caption,
+            "likes": val.likes,
+            "submission": val.submission
+        })
+    random.shuffle(gallery)
+    return JsonResponse({'gallery': gallery}, encoder=ExtendedEncoder)
 
 
 # Create your views here.
 def EntryPageView(req):
-    gallery = (list(Gallery.objects.exclude(entry_id="0").order_by('-likes')))
-    for index,val in enumerate(gallery):
-        val.standing=index
-    random.shuffle(gallery)
-    context = {'leaderboard': gallery}
-    return render(req, 'gallery/entry.html', context)
+    return render(req, 'gallery/entry.html')
 
 
 def updateTheDB():
-    cursor=""
-    entries={}
+    cursor = ""
+    entries = {}
     while True:
-        n=0
-        data = requests.get('https://www.instagram.com/graphql/query/?query_id=17888483320059182&id=39236369448&first=50&after='+cursor)
+        n = 0
+        data = requests.get(
+            'https://www.instagram.com/graphql/query/?query_id=17888483320059182&id=39236369448&first=50&after='
+            + cursor)
         data = data.json()
 
-        for detail in data['data']['user']['edge_owner_to_timeline_media']['edges']:
+        for detail in data['data']['user']['edge_owner_to_timeline_media'][
+                'edges']:
             detail = detail['node']
-            caption = detail['edge_media_to_caption']['edges'][0]['node']['text'][:10]
+            caption = detail['edge_media_to_caption']['edges'][0]['node'][
+                'text'][:10]
             likes = detail['edge_media_preview_like']['count']
             entries[caption] = likes
 
-        pointer = data['data']['user']['edge_owner_to_timeline_media']['page_info']
-        if(pointer['has_next_page']):
+        pointer = data['data']['user']['edge_owner_to_timeline_media'][
+            'page_info']
+        if (pointer['has_next_page']):
             cursor = pointer['end_cursor']
         else:
             break
 
     with transaction.atomic():
-        for entry,likes in entries.items():
+        for entry, likes in entries.items():
             Gallery.objects.filter(entry_id=entry).update(likes=likes)
-    
 
 
 def LeaderBoardView(req):
